842d7663fc7fce436e5309e35f5ea2e4d6b97c2f
Karimov R <rkarimov.r@gmail.com>
2018-08-15 16:47:14 +0400

    Setup rabbitmq message broker

---
+++ gamification/src/main/java/microservices/book/gamification/event/EventHandler.java
@@ -0,0 +1,31 @@
+package microservices.book.gamification.event;
+
+import lombok.extern.slf4j.Slf4j;
+import microservices.book.gamification.service.GameService;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+class EventHandler {
+    private GameService gameService;
+
+    public EventHandler(final GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @RabbitListener(queues = "${multiplication.queue}")
+    void handleMultiplicationSolved(final MultiplicationSolvedEvent event) {
+        log.info("Multiplication Solved Event received: {}",
+                event.getMultiplicationResultAttemptId());
+        try {
+            gameService.newAttemptForUser(event.getUserId(),
+                    event.getMultiplicationResultAttemptId(),
+                    event.isCorrect());
+        } catch (final Exception e) {
+            log.error("Error when trying to process MultiplicationSolvedEvent", e);
+            throw new AmqpRejectAndDontRequeueException(e);
+        }
+    }
+}
