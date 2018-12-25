071a17752cdcf62476c02c0733cf7c8da0497ba6
Rustam Karimov <rkarimov.r@gmail.com>
2018-12-25 19:55:42 +0400

    Implement load balancing with Ribbon

--- social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,24 +3,30 @@ package microservices.book.multiplication.controller;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/results")
 final class MultiplicationResultAttemptController {
 
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
     @Autowired
     MultiplicationResultAttemptController(
-            final MultiplicationService multiplicationService) {
+            final MultiplicationService multiplicationService,
+            @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
     @PostMapping
@@ -44,6 +50,7 @@ final class MultiplicationResultAttemptController {
     @GetMapping("/{resultId}")
     ResponseEntity<MultiplicationResultAttempt> getResultById(
             final @PathVariable("resultId") Long resultId) {
+        log.info("Retrieving result {} from server @{}", resultId, serverPort);
         return ResponseEntity.ok(multiplicationService.getResultById(resultId));
     }
 
