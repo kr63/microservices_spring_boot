2adc2aa58731df5e16a0e79be029e824fc68356b
Rustam Karimov <rkarimov.r@gmail.com>
2018-12-27 22:27:47 +0400

    Write cucumber tests for multiplication microservices

---
+++ tests-e2e/src/test/java/microservices/book/testutils/beans/User.java
@@ -0,0 +1,12 @@
+package microservices.book.testutils.beans;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class User {
+
+    private Long id;
+    private String alias;
+}
